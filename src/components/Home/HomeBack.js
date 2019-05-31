84eab62eeab4e4e19babd08d272cc42b5076ecfc
github username <github email address>
2019-05-31 10:34:54 -0600

    finish services

--- src/components/Home/HomeBack.js
+++
@@ -1,6 +0,0 @@
-<div className="home">
-  <div id="image-container">
-    <img id="home-image" src={img3} alt="..." />
-    <p className="home-center">I am a dedicated pet-sitter who loves animals. I am experienced and I hope I can fulfill your pet's needs.  Your beloved animal is my first priority and I hope I can make the best experience possible for both you and your companion.  I am flexible, and I hope I can take care of any animal from your canary to your cocker spaniel.</p>
-  </div>
-</div>
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -5,8 +5,85 @@ const Services = () => {
 
   return(
     <div className="services">
-      <h1 className="services-center">Welcome to Keilana's Pets & Co!</h1>
-      <p className="services-center">Services</p>
+
+          <div class="services-card" id="one">
+            <div class="services-card-inner">
+              <div class="services-card-front">
+                <div className="fade-card">
+                <h1>Small Dogs</h1>
+                </div>
+              </div>
+              <div class="services-card-back">
+                <h3>(Yorkies, Chihuahuas, etc)</h3>
+                <h4>$15 per day</h4>
+              </div>
+            </div>
+          </div>
+          <div class="services-card" id="two">
+            <div class="services-card-inner">
+              <div class="services-card-front">
+                <div className="fade-card">
+                <h1>Medium Dogs</h1>
+                </div>
+              </div>
+              <div class="services-card-back">
+                <h3>(Yodel Dogs, Schnauzers, etc)</h3>
+                <h4>$20 per day</h4>
+              </div>
+            </div>
+          </div>
+          <div class="services-card" id="three">
+            <div class="services-card-inner">
+              <div class="services-card-front">
+                <div className="fade-card">
+                <h1>Large Dogs</h1>
+                </div>
+              </div>
+              <div class="services-card-back">
+                <h3>(Great Danes Mastiffs, etc)</h3>
+                <h4>$25 per day</h4>
+              </div>
+            </div>
+          </div>
+          <div class="services-card" id="four">
+            <div class="services-card-inner">
+              <div class="services-card-front">
+                <div className="fade-card">
+                <h1>Cats</h1>
+                </div>
+              </div>
+              <div class="services-card-back">
+                <h3>(All cats)</h3>
+                <h4>$15 per day</h4>
+              </div>
+            </div>
+          </div>
+          <div class="services-card" id="five">
+            <div class="services-card-inner">
+              <div class="services-card-front">
+                <div className="fade-card">
+                <h1>Small Mammals and Reptiles</h1>
+                </div>
+              </div>
+              <div class="services-card-back">
+                <h3>(Hamsters, Snakes, Hedgehogs, etc.)</h3>
+                <h4>$10 per day</h4>
+              </div>
+            </div>
+          </div>
+          <div class="services-card" id="six">
+            <div class="services-card-inner">
+              <div class="services-card-front">
+                <div className="fade-card">
+                <h1>BEST CUSTOMER DISCOUNTS</h1>
+                </div>
+              </div>
+              <div class="services-card-back">
+                <h3>(After 3 visits)</h3>
+                <h4>$5 off per day</h4>
+              </div>
+            </div>
+          </div>
     </div>
   )
 }
