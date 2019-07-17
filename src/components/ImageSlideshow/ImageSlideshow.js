026b8e514eca039590840b362ce0e9e7039a3812
github username <klferenczy@gmail.com>
2019-07-17 13:28:26 -0600

    remove slideshow header

--- src/components/ImageSlideshow/ImageSlideshow.js
+++ src/components/ImageSlideshow/ImageSlideshow.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faChevronCircleLeft, faChevronCircleRight } from '@fortawesome/free-solid-svg-icons'
 import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -25,25 +23,10 @@ export default class ImageSlideshow extends Component {
   render() {
     return(
       <div className="slideshow-background">
-        <header className="slideshow-header">
-          <h2>Life Goal:</h2>
-          <h3>Pet ALL the dogs</h3>
-          <h5>(and cats, snakes, hamsters, etc.)</h5>
-        </header>
         <Carousel className="slider-container" autoPlay>
-
           {this.makeSlides()}
-      </Carousel>
-    </div>
-  )
+        </Carousel>
+      </div>
+    )
   }
 }
-
-//
-// <div className="slider-container">
-// <div className="slider">
-//   <img src={this.state.images[this.state.currentIndex]} alt='...' />
-//     <div className="prev" aria-hidden="true" onClick={this.goToPrevSlide}><FontAwesomeIcon icon={faChevronCircleLeft} size="2x" /></div>
-//     <div className="next" aria-hidden="true" onClick={this.goToNextSlide}><FontAwesomeIcon icon={faChevronCircleRight} size="2x" /></div>
-// </div>
-// </div>
